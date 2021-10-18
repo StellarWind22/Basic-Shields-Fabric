bcaabadcf828be41dce371b6def8bc1c77e8d929
CrimsonDawn45 <gannonburks45@gmail.com>
2021-10-18 06:10:19 -0500

    Made shields not all use same model

--- src/main/java/com/github/crimsondawn45/vanillaplusshields/mixin/RendererMixin.java
+++ src/main/java/com/github/crimsondawn45/vanillaplusshields/mixin/RendererMixin.java
@@ -23,21 +23,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BuiltinModelItemRenderer.class)
 public class RendererMixin {
 
-	private ShieldEntityModel modelTestShield;
-
 	//Wood
+	private ShieldEntityModel woodShieldModel;
 	private static final SpriteIdentifier WOOD_SHIELD_BASE = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier(VanillaPlusShields.MOD_ID,"entity/wood_shield_base"));
 	private static final SpriteIdentifier WOOD_SHIELD_BASE_NO_PATTERN = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier(VanillaPlusShields.MOD_ID,"entity/wood_shield_base_nopattern"));
 
 	//Gold
+	private ShieldEntityModel goldShieldModel;
 	private static final SpriteIdentifier GOLD_SHIELD_BASE = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier(VanillaPlusShields.MOD_ID,"entity/gold_shield_base"));
 	private static final SpriteIdentifier GOLD_SHIELD_BASE_NO_PATTERN = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier(VanillaPlusShields.MOD_ID,"entity/gold_shield_base_nopattern"));
 
 	//Diamond
+	private ShieldEntityModel diamondShieldModel;
 	private static final SpriteIdentifier DIAMOND_SHIELD_BASE = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier(VanillaPlusShields.MOD_ID,"entity/diamond_shield_base"));
 	private static final SpriteIdentifier DIAMOND_SHIELD_BASE_NO_PATTERN = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier(VanillaPlusShields.MOD_ID,"entity/diamond_shield_base_nopattern"));
 
 	//Netherrite
+	private ShieldEntityModel netherriteShieldModel;
 	private static final SpriteIdentifier NETHERRITE_SHIELD_BASE = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier(VanillaPlusShields.MOD_ID,"entity/netherrite_shield_base"));
 	private static final SpriteIdentifier NETHERRITE_SHIELD_BASE_NO_PATTERN = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier(VanillaPlusShields.MOD_ID,"entity/netherrite_shield_base_nopattern"));
 
@@ -47,19 +49,19 @@ public class RendererMixin {
 
 
 	@Inject(method = "reload", at = @At("HEAD"))
-	private void setModelTestShield(CallbackInfo ci){
+	private void setModels(CallbackInfo ci){
 
 		//Wood
-		modelTestShield = new ShieldEntityModel(this.entityModelLoader.getModelPart(VanillaPlusShields.wood_shield_model_layer));
+		woodShieldModel = new ShieldEntityModel(this.entityModelLoader.getModelPart(VanillaPlusShields.wood_shield_model_layer));
 
 		//Gold
-		modelTestShield = new ShieldEntityModel(this.entityModelLoader.getModelPart(VanillaPlusShields.gold_shield_model_layer));
+		goldShieldModel = new ShieldEntityModel(this.entityModelLoader.getModelPart(VanillaPlusShields.gold_shield_model_layer));
 
 		//Diamond
-		modelTestShield = new ShieldEntityModel(this.entityModelLoader.getModelPart(VanillaPlusShields.diamond_shield_model_layer));
+		diamondShieldModel = new ShieldEntityModel(this.entityModelLoader.getModelPart(VanillaPlusShields.diamond_shield_model_layer));
 
 		//Netherrite
-		modelTestShield = new ShieldEntityModel(this.entityModelLoader.getModelPart(VanillaPlusShields.netherrite_shield_model_layer));
+		netherriteShieldModel = new ShieldEntityModel(this.entityModelLoader.getModelPart(VanillaPlusShields.netherrite_shield_model_layer));
 	}
 
 	@Inject(method = "render", at = @At("HEAD"))
@@ -67,22 +69,22 @@ public class RendererMixin {
 
 		//Wood
 		if (stack.isOf(VanillaPlusShields.wood_shield)) {
-			FabricShieldLib.renderBanner(stack, matrices, vertexConsumers, light, overlay, modelTestShield, WOOD_SHIELD_BASE, WOOD_SHIELD_BASE_NO_PATTERN);
+			FabricShieldLib.renderBanner(stack, matrices, vertexConsumers, light, overlay, woodShieldModel, WOOD_SHIELD_BASE, WOOD_SHIELD_BASE_NO_PATTERN);
 		}
 
 		//Gold
 		if (stack.isOf(VanillaPlusShields.gold_shield)) {
-			FabricShieldLib.renderBanner(stack, matrices, vertexConsumers, light, overlay, modelTestShield, GOLD_SHIELD_BASE, GOLD_SHIELD_BASE_NO_PATTERN);
+			FabricShieldLib.renderBanner(stack, matrices, vertexConsumers, light, overlay, goldShieldModel, GOLD_SHIELD_BASE, GOLD_SHIELD_BASE_NO_PATTERN);
 		}
 
 		//Diamond
 		if (stack.isOf(VanillaPlusShields.diamond_shield)) {
-			FabricShieldLib.renderBanner(stack, matrices, vertexConsumers, light, overlay, modelTestShield, DIAMOND_SHIELD_BASE, DIAMOND_SHIELD_BASE_NO_PATTERN);
+			FabricShieldLib.renderBanner(stack, matrices, vertexConsumers, light, overlay, diamondShieldModel, DIAMOND_SHIELD_BASE, DIAMOND_SHIELD_BASE_NO_PATTERN);
 		}
 
 		//Netherrite
 		if (stack.isOf(VanillaPlusShields.netherrite_shield)) {
-			FabricShieldLib.renderBanner(stack, matrices, vertexConsumers, light, overlay, modelTestShield, NETHERRITE_SHIELD_BASE, NETHERRITE_SHIELD_BASE_NO_PATTERN);
+			FabricShieldLib.renderBanner(stack, matrices, vertexConsumers, light, overlay, netherriteShieldModel, NETHERRITE_SHIELD_BASE, NETHERRITE_SHIELD_BASE_NO_PATTERN);
 		}
 	}
 }

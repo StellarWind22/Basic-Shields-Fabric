83af98a30a49ab8270549650644555e8c86c2e7c
CrimsonDawn45 <gannonburks45@gmail.com>
2021-10-20 13:36:09 -0500

    Added an itemGroup

--- src/main/java/com/github/crimsondawn45/basicshields/BasicShields.java
+++ src/main/java/com/github/crimsondawn45/basicshields/BasicShields.java
@@ -6,9 +6,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -19,18 +21,19 @@ public class BasicShields implements ModInitializer {
 
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
+	public static ItemGroup MOD_GROUP;
+
 	//Wooden
-	public static final Item wooden_shield = new FabricBannerShieldItem(new FabricItemSettings().maxDamage(80).group(ItemGroup.COMBAT), 110, 15, Items.OAK_PLANKS);
+	public static final Item wooden_shield = new FabricBannerShieldItem(new FabricItemSettings().maxDamage(80).group(MOD_GROUP), 110, 15, Items.OAK_PLANKS);
 
 	//Golden
-	public static final Item golden_shield = new FabricBannerShieldItem(new FabricItemSettings().maxDamage(43).group(ItemGroup.COMBAT), 90, 22, Items.GOLD_INGOT);
+	public static final Item golden_shield = new FabricBannerShieldItem(new FabricItemSettings().maxDamage(43).group(MOD_GROUP), 90, 22, Items.GOLD_INGOT);
 
 	//Diamond
-	public static final Item diamond_shield = new FabricBannerShieldItem(new FabricItemSettings().maxDamage(2097).group(ItemGroup.COMBAT), 90, 10, Items.DIAMOND);
+	public static final Item diamond_shield = new FabricBannerShieldItem(new FabricItemSettings().maxDamage(2097).group(MOD_GROUP), 90, 10, Items.DIAMOND);
 
 	//Netherite
-	public static final Item netherite_shield = new FabricBannerShieldItem(new FabricItemSettings().maxDamage(2709).group(ItemGroup.COMBAT).fireproof(), 80, 15, Items.NETHERITE_INGOT);
-
+	public static final Item netherite_shield = new FabricBannerShieldItem(new FabricItemSettings().maxDamage(2709).group(MOD_GROUP).fireproof(), 80, 15, Items.NETHERITE_INGOT);
 	
 
 	@Override
@@ -47,6 +50,9 @@ public class BasicShields implements ModInitializer {
 
 		//Netherite
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "netherite_shield"), netherite_shield);
+
+		//Item Group
+		MOD_GROUP = FabricItemGroupBuilder.create(new Identifier(MOD_ID, "shields")).icon(() -> new ItemStack(diamond_shield)).build();
 
 		LOGGER.info("Basic Shields initialized!");
 	}

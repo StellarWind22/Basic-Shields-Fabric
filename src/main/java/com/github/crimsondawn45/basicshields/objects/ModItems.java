e7e111979500dbfe808087edebd9f46c5e70bcee
CrimsonDawn45 <gannonburks45@gmail.com>
2021-10-20 15:00:59 -0500

    Started integrating techreborn. try again later

--- src/main/java/com/github/crimsondawn45/basicshields/objects/ModItems.java
+++ src/main/java/com/github/crimsondawn45/basicshields/objects/ModItems.java
@@ -9,10 +9,17 @@ import net.minecraft.item.Items;
 
 public class ModItems {
 
+	//Regular Items
     public static ModItem wooden_shield;
 	public static ModItem golden_shield;
 	public static ModItem diamond_shield;
 	public static ModItem netherite_shield;
+
+	//Tech Reborn Items
+	//public static ModItem bronze_shield;
+	//public static ModItem ruby_shield;
+	//public static ModItem saphire_shield;
+	//public static ModItem peridot_shield;
 
     public static void Init() {
         wooden_shield = new ModItem("wooden_shield", new FabricBannerShieldItem(new FabricItemSettings().maxDamage(80).group(BasicShields.SHIELDS), 110, 15, Items.OAK_PLANKS));
@@ -20,4 +27,10 @@ public class ModItems {
 		diamond_shield = new ModItem("diamond_shield", new FabricBannerShieldItem(new FabricItemSettings().maxDamage(2097).group(BasicShields.SHIELDS), 90, 10, Items.DIAMOND));
 		netherite_shield = new ModItem("netherite_shield", new FabricBannerShieldItem(new FabricItemSettings().maxDamage(2709).group(BasicShields.SHIELDS).fireproof(), 80, 15, Items.NETHERITE_INGOT));
     }
+
+	/*
+	public static void TechRebornInit() {
+		
+	}
+	*/
 }

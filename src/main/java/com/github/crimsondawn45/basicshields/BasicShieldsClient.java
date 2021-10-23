10cf11a81d57def68590985298468dfc4c2a9816
CrimsonDawn45 <gannonburks45@gmail.com>
2021-10-23 13:13:45 -0500

    ported 1.0.2 to 1.15

--- src/main/java/com/github/crimsondawn45/basicshields/BasicShieldsClient.java
+++ src/main/java/com/github/crimsondawn45/basicshields/BasicShieldsClient.java
@@ -11,27 +11,29 @@ public class BasicShieldsClient implements ClientModInitializer {
     public void onInitializeClient() {
 
         //Wooden
-        ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE).register((atlasTexture, registry) -> {
+        ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEX).register((atlasTexture, registry) -> {
             registry.register(new Identifier(BasicShields.MOD_ID, "entity/wooden_shield_base"));
             registry.register(new Identifier(BasicShields.MOD_ID, "entity/wooden_shield_base_nopattern"));
         });
 
         //Golden
-        ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE).register((atlasTexture, registry) -> {
+        ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEX).register((atlasTexture, registry) -> {
             registry.register(new Identifier(BasicShields.MOD_ID, "entity/golden_shield_base"));
             registry.register(new Identifier(BasicShields.MOD_ID, "entity/golden_shield_base_nopattern"));
         });
 
         //Diamond
-        ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE).register((atlasTexture, registry) -> {
+        ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEX).register((atlasTexture, registry) -> {
             registry.register(new Identifier(BasicShields.MOD_ID, "entity/diamond_shield_base"));
             registry.register(new Identifier(BasicShields.MOD_ID, "entity/diamond_shield_base_nopattern"));
         });
 
         //Netherite
+        /*
         ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE).register((atlasTexture, registry) -> {
             registry.register(new Identifier(BasicShields.MOD_ID, "entity/netherite_shield_base"));
             registry.register(new Identifier(BasicShields.MOD_ID, "entity/netherite_shield_base_nopattern"));
         });
+        */
     }
 }
